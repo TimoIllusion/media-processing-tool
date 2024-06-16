42058312f305eb0c2c6dc3c06317192276fb59f6
Timo Leitritz <42964574+TimoIllusion@users.noreply.github.com>
2024-06-16 14:24:22 +0200

    Restructure

--- flask-no-docker/backend/config.py
+++
@@ -1,8 +0,0 @@
-class BackendConfig:
-    STORAGE_ACCESS_KEY="minioadmin"
-    STORAGE_SECRET_KEY="minioadmin"
-    STORAGE_HOST = "localhost:9000"
-    INPUT_BUCKET_NAME = "video-uploads"
-    OUTPUT_BUCKET_NAME = "output"
-    FRONTEND_URL = "http://localhost:5000"
-    PORT=5001
--- flask-no-docker/frontend/config.py
+++
@@ -1,7 +0,0 @@
-class FrontendConfig:
-    STORAGE_ACCESS_KEY="minioadmin"
-    STORAGE_SECRET_KEY="minioadmin"
-    STORAGE_HOST = "localhost:9000"
-    INPUT_BUCKET_NAME = "video-uploads"
-    OUTPUT_BUCKET_NAME = "output"
-    BACKEND_URL = "http://localhost:5001"
